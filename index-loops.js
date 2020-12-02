3246bc5ab3ec35b12b92f33474818a0546ef18e7
Bgibbemeyer <gibbemeyer85@gmail.com>
2020-12-02 12:21:50 -0500

    file name changes

--- index-loops.js
+++ index-loops.js
@@ -6,17 +6,21 @@ while (a <= 100) {
     console.log(a); 
   }
 }
+console.log("");
 
-let b = 100
+let b = 103
 
-while (b >= 0) {
+while (b >= 3) {
     b-=3;
     console.log(b);
 }
+console.log("");
 
 for (var i = 1; i <= 100; i+=2) {
     console.log(i);
  }
+ console.log("");
+
 
  for (var d = 0; d <= 100; d++) {
      if (!(d % 3)) {
